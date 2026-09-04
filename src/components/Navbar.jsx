6f9a9c24c5e12e648a65c3bef6c1f68a59c0d310
SMMathias <mathias08102004@gmail.com>
2026-09-04 10:58:04 +0200

    flyttet navbar styles til dens module.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import { NavLink } from "react-router";
+import styles from "./Navbar.module.css";
+
+const linkClass = ({ isActive }) => (isActive ? styles.active : undefined);
 
 export default function Navbar() {
   return (
-    <nav className="site-nav">
-      <NavLink className="brand" to="/">
+    <nav className={styles.siteNav}>
+      <NavLink className={styles.brand} to="/">
         mellemrum<span>.</span>
       </NavLink>
-      <div className="nav-links">
-        <NavLink to="/">Events</NavLink>
-        <NavLink to="/om">Om Mellemrum</NavLink>
+      <div className={styles.navLinks}>
+        <NavLink className={linkClass} to="/">Events</NavLink>
+        <NavLink className={linkClass} to="/om">Om Mellemrum</NavLink>
       </div>
     </nav>
   );

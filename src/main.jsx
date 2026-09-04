77fc6ec94e15113fd164a0b306d294d8c08b7156
SMMathias <mathias08102004@gmail.com>
2026-09-04 11:06:09 +0200

    fjernet unødvendig CSS-import og oppdatert stilark med nye klasser for bedre struktur

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles/variables.css";
 import "./styles/root.css";
-import "./styles.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(

3081b644511af157a11f4310542b1213d808f4b5
SMMathias <mathias08102004@gmail.com>
2026-09-04 10:55:26 +0200

    flytet base styles til root.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles/variables.css";
+import "./styles/root.css";
 import "./styles.css";
 import App from "./App.jsx";
 
